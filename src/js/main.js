39f6c18253b987d5076fd0606e0881443c53c294
OlegPeliak <op@branchup.pro>
2019-01-11 10:14:10 +0200

    create first tab

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,30 @@
 
 
 $('document').ready(function(){
+    $('.directory-list .main-list, .reception-actions-block li').click(function () {
+        $(this).toggleClass('add-active');
+    });
+
+    $('.directory-list li .list-edit').click(function () {
+        var parent = $(this).parents('.directory-list li');
+        $(this).toggleClass('active');
+        $(parent).toggleClass('more-info');
+        if ($(parent).hasClass('more-info')) {
+            $(parent).append('<span class="more-span"><input type="text" name="more-input" class="more-input"></span>');
+            $(parent).addClass('add-active');
+        } else{
+            $('.more-span').remove();
+        }
+    });
+
+   $('body,html').on('click', function (e) {
+        if (!$(e.target).closest('.directory-list li').length){
+            $('.more-span').remove();
+            $('.directory-list li').removeClass('more-info');
+            $('.directory-list li .list-edit').removeClass('active');
+        }
+    });
+
 
 });
 
